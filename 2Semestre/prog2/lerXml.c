eed27b32a6b552e27762b9b365e95caf2864fec3
JPeeeeee <joaopedrodev8@gmail.com>
2022-11-09 13:11:02 -0300

    [ADD] Free no vetor de strings

--- 2Semestre/prog2/lerXml.c
+++ 2Semestre/prog2/lerXml.c
@@ -21,32 +21,38 @@ int encontrarTag (char ch, FILE* arq, char *chave){
     return 1;
 }
 
-void sumarizarPeriodicosOuRevistas(FILE *arq){
+char** vetorizaStrings(FILE *arq){
     char ch;
-    char *str = malloc(sizeof(char) * 512);
-    char** periodicoOuRevista = malloc(sizeof(str) * 512);
+    char *str = malloc(sizeof(char) * 512); // string auxiliar
+    char** periodicoOuRevista = malloc(sizeof(str) * 512); // Vetor de strings
     int i = 0;
+
     // Enquanto o arquivo nao acabar
-     while (ch != EOF) {
-        ch = fgetc(arq);
-        
+    while (ch != EOF) {
+        ch = fgetc(arq); // Chama o proximo char
+
         // Encontra a a propriedade periodico ou revista no arquivo XML
         if (encontrarTag(ch, arq, "STA=")){
-            ch = fgetc(arq);
+            ch = fgetc(arq); // Pula a primeira aspa após o sinal de igualdade
 
             while (ch != '\"'){
-                strncat(str, &ch, 1);
-
-                ch = fgetc(arq);
+                strncat(str, &ch, 1); // Concatena o char na string str
+                ch = fgetc(arq); // Pega o proximo char
             }
             
-            periodicoOuRevista[i] = malloc(sizeof(char) * 512);
-            strcpy(periodicoOuRevista[i], str);
+            periodicoOuRevista[i] = malloc(sizeof(char) * 512); // Aloca espaco para a string dentro do vetor
+            strcpy(periodicoOuRevista[i], str); // copia a string para dentro do vetor
             printf("string: %s\n", periodicoOuRevista[i]);
-            i++;
-            strcpy(str, "");
+            i++; // itera "i", o numero de elementos no vetor
+            strcpy(str, ""); // limpa a string str
         }
     };
+
+    for (int j = 0; j < i; j++)
+        free(periodicoOuRevista)
+
+    free(str);
+
 }
 
 // fazer funcao especifica para comparar strings usando encontrar tag
@@ -54,7 +60,7 @@ void sumarizarPeriodicosOuRevistas(FILE *arq){
 int main()
 {
     FILE* arq;
-
+    char **periodicosOuRevistas;
 
     // Abre o arquivo em modo de leitura
     arq = fopen("curriculo.xml", "r");
@@ -66,8 +72,7 @@ int main()
     }
 
     // Acessa coloca todos os periodicos ou revistas em uma variavel
-    sumarizarPeriodicosOuRevistas(arq);
- 
+    periodicosOuRevistas = vetorizaStrings(arq);
     // Fecha arquivo
     fclose(arq);
     return 0;
