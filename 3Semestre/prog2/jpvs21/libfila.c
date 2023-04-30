6955273392f61b4ae5ac0116a28b6142523f1202
JPeeeeee <joaopedrodev8@gmail.com>
2023-04-30 17:00:48 -0300

    [FIX] adiciona chave ajustado

--- 3Semestre/prog2/jpvs21/libfila.c
+++ 3Semestre/prog2/jpvs21/libfila.c
@@ -43,28 +43,6 @@ int adiciona_chave (fila_t *f, char c, int chave) {
     return 1;
 }
 
-// int adiciona_chave (fila_t *f, char c, int chave) {
-
-//     nodo_f_t *aux = f->ini;
-
-//     while (aux->letra != c) {
-//         aux = aux->prox;
-//     }
-
-//     if (aux->chaves == NULL){
-//         aux->chaves = malloc(sizeof(int));
-//         aux->chaves[0] = chave;
-//         aux->tamanho++;
-//         return 1;
-//     } else{
-//         aux->tamanho++;
-//         aux->chaves = realloc(aux->chaves, sizeof(int) * aux->tamanho);
-//         aux->chaves[aux->tamanho - 1] = chave;
-//         return 1;
-//     }
-//     return 0;
-// }
-
 int insere_fila (fila_t* f, char c, int chave) {
 
     if (busca_fila(f, c)){
