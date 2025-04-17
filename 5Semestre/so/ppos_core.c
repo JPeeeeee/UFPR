94b033f470b949403bd2fb7f34fc1de7eb68fd75
JPeeeeee <joaopedrodev8@gmail.com>
2025-04-17 13:41:38 -0300

    [ADD] trabalho P2 so

--- 5Semestre/so/ppos_core.c
+++ 5Semestre/so/ppos_core.c
@@ -1,14 +1,29 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <ucontext.h>
 #include "ppos.h"
 
 #define STACKSIZE 64*1024	/* tamanho de pilha das threads */
 
+static task_t *CurrentTask;
+static task_t MainTask;
+static int taskCounter = 0;
+
+
+# define PRONTA 0
+# define EXECUTANDO 1
+# define TERMINADA 2
 
 void ppos_init() {
     setvbuf (stdout, 0, _IONBF, 0) ;
-    getcontext(&ContextMain);
-    ContextCurrent = ContextMain;
+    
+
+    getcontext(&(MainTask.context));
+    MainTask.id = 0;
+    MainTask.status = PRONTA;
+    MainTask.prev = NULL;
+    MainTask.next = NULL;
+    CurrentTask = &MainTask;
 }
 
 int task_init (task_t *task, void (*start_routine)(void *),  void *arg) {
@@ -28,10 +43,37 @@ int task_init (task_t *task, void (*start_routine)(void *),  void *arg) {
        exit (1) ;
     }
 
-    makecontext(&(task->context), start_routine, 1, arg);
+    taskCounter++;
+    task->id = taskCounter;
+
+    task->status = PRONTA;
+
+    makecontext(&(task->context), (void (*)(void))start_routine, 1, arg);
+
+    return task->id;
 }
 
 int task_switch (task_t *task) {
-    swapcontext(&ContextCurrent, &(task->context));
-    
+    if (task == NULL) 
+	return -1;
+
+    task_t *oldTask = CurrentTask;
+
+    if (oldTask != &MainTask)
+	oldTask->status = PRONTA;
+
+    CurrentTask = task;
+    task->status = EXECUTANDO;
+
+    swapcontext(&(oldTask->context), &(task->context));
+    return 0;
+}
+
+void task_exit(int exit_code) {
+    CurrentTask->status = TERMINADA;
+    task_switch(&MainTask);
+}
+
+int task_id() {
+    return CurrentTask->id;
 }
